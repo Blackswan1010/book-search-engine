e5a80f8cd46180c6aec706833cd3e09ada5ffb1d
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-07 05:30:21 -0800

    imported models and utils, implemented query and mutation

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { Book, User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, { username }) => {
+            return User.findOne({ username: username }).populate('savedBooks');
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });     
+            if (!user) {
+                throw AuthenticationError;
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { bookToSave, username}) => {
+            return User.findOneAndUpdate(
+                { username: username },
+                { $addToSet: { savedBooks: bookToSave }},
+                { new: true, }
+            );
+        },
+        removeBook: async (parent, { bookId, username }) => {
+            return User.findOneAndUpdate(
+                { username: username},
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+            );
+        },
+    }
+};
+
+module.exports = resolvers;
