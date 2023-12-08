4f26c36fdaf372d3978434bfeec5bcaeb69cb99c
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-08 00:00:46 -0800

    minor fixes and formatting

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -11,7 +11,7 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
-  const [login, {}] = useMutation(LOGIN_USER);
+  const [login, { data, error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -33,7 +33,7 @@ const LoginForm = () => {
         variables: { ...userFormData },
       });
 
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
